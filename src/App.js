61cbcf7032a8504af9ad6e1dd753ce6159913693
Majelvey <mckelvey.andy@gmail.com>
2022-07-15 15:36:48 -0400

    Added additional required components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/Footer";
 
 
-const App = () => <PortfolioContainer />;
+function App() {
+  return (
+    <div>
+      <Header />
+      <PortfolioContainer />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const styles = {
+  h1: {
+    background: 'green',
+  }
+}
+
+function Footer() {
+  return (
+    <div>
+      <h1 style={styles.h1}>Footer Content</h1>
+      
+    </div>
+  );
+}
+
+export default Footer;
