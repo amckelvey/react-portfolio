0b16a3b980b30975555669b2de559f186cc9b47c
Majelvey <mckelvey.andy@gmail.com>
2022-07-15 15:46:31 -0400

    got Header component loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 
 function App() {
